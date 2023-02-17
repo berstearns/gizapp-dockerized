0a3ec8d13af079b86c534be5eb3cb152abfe2bd0
Bernardo Stearns <bernardostearns@DSIs-MacBook-Pro-2.local>
2023-02-17 14:40:24 +0000

    adding tsv to parallel data conversiion, and tested datasets

--- testing_giza.sh
+++ testing_giza.sh
@@ -1,5 +1,29 @@
-#!/bin/zsh
+#!/bin/sh
+n1="en"
+n2="ge"
+folder_name="our"
 cd GIZA++-v2/
-./plain2snt.out ../giza_example/data.en ../giza_example/data.es 
-./snt2cooc.out ../giza_example/data.en.vcb ../giza_example/data.es.vcb ../giza_example/data.en_data.es.snt > ../giza_example/corp.cooc
-./GIZA++ -S ../giza_example/data.es.vcb -T ../giza_example/data.en.vcb -C ../giza_example/data.en_data.es.snt -CoocurrenceFile ../giza_example/corp.cooc -outputpath ../giza_example/giza_output
+./plain2snt.out ../giza_example/$folder_name/data.$n1 ../giza_example/$folder_name/data.$n2 
+mv ../giza_example/$folder_name/data.$n1.vcb ../giza_example/$folder_name/outputs/data.$n1.vcb 
+mv ../giza_example/$folder_name/data.$n2.vcb ../giza_example/$folder_name/outputs/data.$n2.vcb 
+mv "../giza_example/${folder_name}/data.${n1}_data.${n2}.snt" ../giza_example/$folder_name/outputs/data.$n1.snt 
+mv "../giza_example/${folder_name}/data.${n2}_data.${n1}.snt" ../giza_example/$folder_name/outputs/data.$n2.snt 
+
+./snt2cooc.out "../giza_example/$folder_name/outputs/data.$n1.vcb"\
+	       "../giza_example/$folder_name/outputs/data.$n2.vcb"\
+	       "../giza_example/$folder_name/outputs/data.$n2.snt" > ../giza_example/$folder_name/outputs/corp.cooc
+
+cd ..
+cd ./mkcls-v2/
+./mkcls -p"../giza_example/$folder_name/outputs/data.$n1.vcb"\
+       -V"../giza_example/$folder_name/outputs/data.$n1.vcb.classes"
+
+./mkcls -p"../giza_example/$folder_name/outputs/data.$n2.vcb"\
+        -V"../giza_example/$folder_name/outputs/data.$n2.vcb.classes"
+
+cd .. 
+cd GIZA++-v2/
+./GIZA++ -S "../giza_example/$folder_name/outputs/data.$n2.vcb"\
+	 -T "../giza_example/$folder_name/outputs/data.$n1.vcb"\
+         -C "../giza_example/$folder_name/outputs/data.$n1.snt"\
+	 -CoocurrenceFile ../giza_example/corp.cooc 
